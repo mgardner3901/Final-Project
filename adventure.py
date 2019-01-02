beb54ed1b9c69637cd1e18c02905a4666fdb4ce2
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2019-01-01 21:13:03 -0500

    Updated from Brython Server: 1/1/2019 9:13:02 PM

--- adventure.py
+++ adventure.py
@@ -21,11 +21,106 @@ else:
 print ("As you proceed further into the cave, you see a small glowing object")
 ch2 = str(input("Do you approach the object? [y/n]"))
 
-if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
+
+    if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
     print ("You approach the object...")
     print ("As you draw closer with the help of your candle you begin to make out what the shine is.")
     print ("The eye belongs to a goblin!")
     print("It looks as if he is munching on the arm of what you can only assume to be one of the previous guards of this prison.")
     print("It also looks as if he is holding on to something shiny.")
     ch3 = str(input("Do you try to fight it? [Y/N]"))
+    
+    if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
+
+        #with candle
+        if candle == 1:
+            print ("You only have a candle to fight with!")
+            print ("You wave the flame at the goblin and it screeches and cowers")
+            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print ("                  Fighting...                   ")
+            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
+            print ("IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
+            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            fdmg1 = int(random.randint(3, 9))
+            edmg1 = int(random.randint(1, 5))
+            print ("you hit a", fdmg1)
+            print ("the goblin hits a", edmg1)
+
+            if edmg1 > fdmg1:
+                print ("The goblin has dealt more damage than you!")
+                complete = 0
+                return complete
+
+            elif fdmg1 < 5:
+                print ("You didn't do enough damage to kill the goblin, but you manage to fend it off")
+                complete = 1
+                return complete
+
+            else:
+                print ("You killed the goblin!")
+                complete = 1
+                return complete
+
+        #without candle
+        else:
+            print ("You don't have anything to fight with!")
+            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print ("                  Fighting...                   ")
+            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
+            print ("IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
+            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            fdmg1 = int(random.randint(2, 8))
+            edmg1 = int(random.randint(1, 5))
+            print ("you hit a", fdmg1)
+            print ("the goblin hits a", edmg1)
+
+            if edmg1 > fdmg1:
+                print ("The goblin has dealt more damage than you!")
+                complete = 0
+                return complete
+
+            elif fdmg1 < 5:
+                print ("You didn't do enough damage to kill the goblin, but you manage to escape")
+                complete = 1
+                return complete
+
+            else:
+                print ("You killed the goblin!")
+                complete = 1
+                return complete
+
+    #dont fight
+    print ("You choose not to fight the goblin.")
+    print ("As you turn to run away it rushes you and stabs you with it's dagger!")
+    complete = 0
+    return complete
+
+#dont approach
+else:
+        print ("You turn away from the glowing object, and attempt to leave the cave...")
+        time.sleep(1)
+        print ("But something won't let you....")
+        complete = 0
+        return complete
+
+# game loop
+alive = True
+while alive:
+
+complete = game()
+if complete == 1:
+    alive = input('You managed to escape the cavern alive! Would you like to play again? [y/n]: ')
+    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
+        alive
+
+    else:
+        break
+
+else:
+    alive = input('You have died! Would you like to play again? [y/n]: ')
+    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
+        alive
+
+    
+
 
