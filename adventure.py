9ded76276de5aa811b5fd1ae0608ccd0559f9c89
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2019-01-09 23:50:13 -0500

    Updated from Brython Server: 1/9/2019 11:50:12 PM

--- adventure.py
+++ adventure.py
@@ -1,18 +1,17 @@
 """
 Author: Morgan Gardner
-Credit: codereview.stackexchange.com
+Credit:  codereview.stackexchange.com
 """    
     
-print('-----------------')
-print('   -Adventure-   ')
-print('-----------------')
+
+print('                                         -----Adventure-----   ')
 
 import random
 
 print ("You wake up in a dark room lit only by a single cande. Your eyes adjust and you look around. You seem to be in a cell made from stone and you notice that the door to the cell has been left open. You decide to get up and explore. Do you take the candle with you? ")
-ch1 = str(input("Do you take it? [Yes or No]: "))
+case1 = str(input("Do you take it? [Yes or No]: "))
 
-if ch1 in ['y', 'Y', 'Yes', 'YES', 'yes']:
+if case1 in ['y', 'Y', 'Yes', 'YES', 'yes']:
     print("You have taken the candle")
     candle = 1
 
@@ -21,29 +20,31 @@ else:
     candle = 0
     
 print ("As you proceed further into the cave, you see a small glowing object")
-ch2 = str(input("Do you approach the object? [Yes or No]"))
+case2 = str(input("Do you approach the object? [Yes or No]"))
 
-if ch2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
+if case2 in ['n', 'N', 'No', 'NO', 'No']:
+    print('You attempt to tuen and go in the opposite direction of the object but whatever it is its alive! It leaps at you!')
+
+if case2 in ['y', 'Y', 'Yes', 'YES', 'yes']:
      print ("You approach the object...")
-     print ("As you draw closer with the help of your candle you begin to make out what the shine is.")
-     print ("The eye belongs to a goblin!")
-     print("It looks as if he is munching on the arm of what you can only assume to be one of the previous guards of this prison.")
+     print ("As you draw closer you begin to make out what the glow is.")
+     print ("Its an eye! And it belongs to a goblin! It looks as if he is munching on the arm of what you can only assume to be one of the previous guards of this prison.")
      print("It also looks as if he is holding on to something shiny.")
-   
-ch3 = str(input("Do you try to fight it? [Yes or No]"))
+     
+case3 = str(input("Do you try to fight it? [Yes or No]"))
  
  
-if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
+if case3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
 
         #with candle
         if candle == 1:
             print ("You only have a candle to fight with!")
             print ("You wave the flame at the goblin and it screeches and cowers")
-            print ("------------------------------------------------")
-            print ("                     COMBAT                     ")
-            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
-            print ("IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
-            print ("------------------------------------------------")
+            print ("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+            print ("                      COMBAT                     ")
+            print ("    YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
+            print (" IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
+            print ("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
             fdmg1 = int(random.randint(3, 9))
             edmg1 = int(random.randint(1, 5))
             print ("you hit a", fdmg1)
@@ -55,7 +56,7 @@ if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
                 return complete
 
             elif fdmg1 < 5:
-                print ("You didn't do enough damage to kill the goblin, but you manage to fend it off")
+                print ("You didn't do enough damage to kill the goblin, but you manage to fend it off and escape")
                 complete = 1
                 return complete
 
@@ -67,11 +68,11 @@ if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
         #without candle
         else:
             print ("You don't have anything to fight with!")
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print ("                  Fighting...                   ")
-            print ("   YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
-            print ("IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
-            print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print ("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+            print ("                    Fighting...                  ")
+            print ("    YOU MUST HIT ABOVE A 5 TO KILL THE GOBLIN    ")
+            print (" IF THE GOBLIN HITS HIGHER THAN YOU, YOU WILL DIE")
+            print ("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
             fdmg1 = int(random.randint(1, 7))
             edmg1 = int(random.randint(1, 5))
             print ("you hit a", fdmg1)
@@ -83,7 +84,7 @@ if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
                 return complete
 
             elif fdmg1 < 5:
-                print ("You didn't do enough damage to kill the goblin, but you manage to escape")
+                print ("You didn't do enough damage to kill the goblin, but you manage to get away")
                 complete = 1
                 return complete
 
@@ -91,50 +92,16 @@ if ch3 in ['y', 'Y', 'Yes', 'YES', 'yes']:
                 print ("You killed the goblin!")
                 complete = 1
                 return complete
+#dont approach                
+if case3 in ['n', 'N', 'No', 'NO', 'no']:
+    print ("You run and attempt to leave the cave...")
+    print ("But something won't let you....")
+    complete = 0
+    return complete
+    
+    
+
                 
-#dont fight
-print ("You choose not to fight the goblin.")
-print ("As you turn to run away it rushes you and stabs you with it's dagger!")
-complete = 0
-return complete
-
-
-else:
-        print ("You turn away from the glowing object, and keep searching for an exit...")
-        print ("But something stops you...")
-        complete = 0
-        return complete
-        
-
-
-alive = True
-while alive:
-
-complete = game()
-if complete == 1:
-    alive = input('You see a bright light through what looks to be a small doorway. You climb up some rubble and squeeze your way out and find yourself in a clearing. You escaped alive! Would you like to play again? [y/n]: ')
-    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
-        alive
-
-    else:
-        break
-
-else:
-    alive = input('GAME OVER! Play again? [y/n]: ')
-    if alive in ['y', 'Y', 'YES', 'yes', 'Yes',]:
-        alive
-
-    else:
-        break
-
-    
-    
-
-
-
-
-
-    
 
 
 
